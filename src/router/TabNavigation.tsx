bda8b197a7c8eba7a5d9d85e900a6745e5d3607f
Pedro Luis D L <Breidydl@gmail.com>
2023-08-15 16:09:12 -0400

    sigue con el tab

--- src/router/TabNavigation.tsx
+++ src/router/TabNavigation.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Home from '../views/Home';
 import Accounts from '../views/Accounts';
@@ -8,23 +8,57 @@ import Login from '../views/Login';
 
 const Tab = createMaterialBottomTabNavigator();
 
-const homeIcon = () => {
-  <MaterialCommunityIcons name="home" color="red" size={26} />;
-};
+const homeIcon = (color: any, iconName: string) => (
+  <Icon name={iconName} color={color} size={30} />
+);
 
 const TabNavigator = () => {
+  const [bgColor, setBgColor] = useState('red');
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      shifting
+      activeColor="blue"
+      barStyle={{ backgroundColor: bgColor }}>
       <Tab.Screen
         name="Login"
         component={Login}
         options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }: any) => homeIcon(color),
+          tabBarIcon: ({ color }) => homeIcon(color, 'abugida-devanagari'),
+          tabBarColor: '#000',
         }}
       />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Accounts" component={Accounts} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => homeIcon(color, 'abjad-arabic'),
+          tabBarColor: 'red',
+        }}
+      />
+      <Tab.Screen
+        name="Accounts"
+        component={Accounts}
+        options={{
+          tabBarIcon: ({ color }) => homeIcon(color, 'abjad-hebrew'),
+          tabBarColor: 'red',
+        }}
+      />
+      <Tab.Screen
+        name="Accounts2"
+        component={Accounts}
+        options={{
+          tabBarIcon: ({ color }) => homeIcon(color, 'abugida-thai'),
+          tabBarColor: 'red',
+        }}
+      />
+      <Tab.Screen
+        name="Accounts3"
+        component={Accounts}
+        options={{
+          tabBarIcon: ({ color }) => homeIcon(color, 'alien'),
+          tabBarColor: 'red',
+        }}
+      />
     </Tab.Navigator>
   );
 };
