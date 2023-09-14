5f12ed73c418bc761f82d92b9714d2170797aff4
Pedro Luis D L <Breidydl@gmail.com>
2023-09-14 16:54:42 -0400

    Arreglar los charts

--- src/components/cards/MetricsCard.tsx
+++ src/components/cards/MetricsCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 // import Icon from 'react-native-vector-icons/FontAwesome';
-import { PieChart } from 'react-native-charts-wrapper';
 
 const fontColor = {
     blanco: '#fff',
@@ -14,38 +13,6 @@ const MetricsCard = ({
     monto_corriente,
     fecha_de_creacion,
 }: any) => {
-    const data = {
-        dataSets: [
-            {
-                values: [
-                    { value: 45, label: 'Sandwiches' },
-                    { value: 21, label: 'Salads' },
-                    { value: 15, label: 'Soup' },
-                    { value: 9, label: 'Beverages' },
-                    { value: 15, label: 'Desserts' },
-                ],
-                label: 'Pie dataset',
-                config: {
-                    colors: [
-                        '#C0FF8C',
-                        '#FFF78C',
-                        '#FFD08C',
-                        '#8CEAFF',
-                        '#FF8C9D',
-                    ],
-                    valueTextSize: 20,
-                    valueTextColor: 'green',
-                    sliceSpace: 5,
-                    selectionShift: 13,
-                    // xValuePosition: "OUTSIDE_SLICE",
-                    // yValuePosition: "OUTSIDE_SLICE",
-                    valueFormatter: "#.#'%'",
-                    valueLineColor: 'green',
-                    valueLinePart1Length: 0.5,
-                },
-            },
-        ],
-    };
     return (
         <View style={styles.container}>
             <View style={styles.dataContainer}>
@@ -72,40 +39,7 @@ const MetricsCard = ({
                     </View>
                 </View>
             </View>
-            <View style={styles.chartContainer}>
-                <PieChart
-                    style={styles.chart}
-                    logEnabled={true}
-                    chartBackgroundColor={'pink'}
-                    chartDescription={'prueba'}
-                    data={data}
-                    legend={'legend'}
-                    highlights={{ x: 2 }}
-                    extraOffsets={{ left: 5, top: 5, right: 5, bottom: 5 }}
-                    entryLabelColor={'green'}
-                    entryLabelTextSize={20}
-                    entryLabelFontFamily={'HelveticaNeue-Medium'}
-                    drawEntryLabels={true}
-                    rotationEnabled={true}
-                    rotationAngle={45}
-                    usePercentValues={true}
-                    // eslint-disable-next-line react-native/no-inline-styles
-                    styledCenterText={{
-                        text: 'Pie center text!',
-                        color: 'pink',
-                        fontFamily: 'HelveticaNeue-Medium',
-                        size: 20,
-                    }}
-                    centerTextRadiusPercent={100}
-                    holeRadius={40}
-                    holeColor={'#f0f0f0'}
-                    transparentCircleRadius={45}
-                    transparentCircleColor={'#f0f0f088'}
-                    maxAngle={350}
-                    // onSelect={this.handleSelect.bind(this)}
-                    onChange={(event: any) => console.log(event.nativeEvent)}
-                />
-            </View>
+            <View style={styles.chartContainer}> </View>
         </View>
     );
 };
